bd32201fd3c75760a722ed5770a7e9b0003762d7
Joseph An <joseph.an@thescore.com>
2017-12-23 00:58:34 -0500

    Initial commit

---
+++ lib/elixir_cowboy.ex
@@ -0,0 +1,18 @@
+defmodule ElixirCowboy do
+  @moduledoc """
+  Documentation for ElixirCowboy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirCowboy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
